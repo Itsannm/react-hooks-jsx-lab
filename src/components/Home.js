f3dc5582f3f372e916a0d895344ec9e4d7f5c4fc
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-07-20 23:48:37 +0300

    Navbar Complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Ochocinco</p>   
+  </div>;
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <>
+  <nav>
+    <ul>
+      <li><a href="./Home">Home</a></li>
+      <li><a href="./About">About</a></li>
+    </ul>
+        </nav>;
+  </>
+  )
 }
 
 export default NavBar;
